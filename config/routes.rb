1aa8260aca331581425c33a0049d0250ea640125
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2017-08-09 19:31:40 -0500

    adds Sessions model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #User Routes 
   get "/signup" => "users#new"
   post "/users" => "users#create"
+
+  #Session Routes
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
